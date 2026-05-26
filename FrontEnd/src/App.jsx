d6a2006026f7204cb06c342553615c157ae59c6d
Anurag Pandey <anuragpandeymanu39@gmail.com>
2026-05-27 02:08:21 +0530

    Integrated Frontend and Backend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 import axios from 'axios'
 import './App.css'
 
@@ -46,10 +49,12 @@ async function reviewCode(){
         onClick={reviewCode}
         className="review">Review</div>
       </div>
-      <div className="right">{
-        review
+      <div className="right">
+        <Markdown
+        rehypePlugins={[rehypeHighlight]}
+        >{review}</Markdown>
         
-}</div>
+</div>
     </main>
     </>
   )

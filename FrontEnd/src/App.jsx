d9743c48e9710c9e2370c24133ae7ddc3718fe79
Anurag Pandey <anuragpandeymanu39@gmail.com>
2026-05-27 20:53:29 +0530

    Enhanced Features

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,11 +5,21 @@ const Editor = _Editor.default ?? _Editor        // ✅ CJS/ESM fix
 import Prism from "prismjs"
 import "prismjs/components/prism-javascript"
 import "prismjs/themes/prism-tomorrow.css"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight"
+import "highlight.js/styles/github-dark.css"
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [code, setCode] = useState(`function sum() {\n  return 1 + 1\n}`)
 
+const [review, setReview] = useState(``)
+
+  async function reviewCode(){
+ const response = await axios.post('http://localhost:3000/ai/get-review', {code})
+  setReview(response.data)
+}
   return (
     <>
       <main>
@@ -34,9 +44,17 @@ function App() {
               }}
             />
           </div>
-          <div className="review">Review</div>
+          <div 
+          onClick={reviewCode}
+          className="review">Review</div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <Markdown
+          
+          rehypePlugins={[rehypeHighlight]}
+          
+          >{review}</Markdown>
+        </div>
       </main>
     </>
   )

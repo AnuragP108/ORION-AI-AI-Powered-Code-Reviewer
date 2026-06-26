0e121bf84a12a9700be45a5a13588bfba52aa550
Anurag Pandey <anuragpandeymanu39@gmail.com>
2026-06-26 19:11:39 +0530

    Dynamic port for render deployment

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,9 @@
-require('dotenv').config()
-const app=require('./src/app')
+require('dotenv').config();
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
-})
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
